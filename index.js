161fd353a84174df9d5c9158184c957c12640d24
Adis Aljic <adis.qm@gmail.com>
2022-11-27 12:15:47 +0100

    added new features

--- index.js
+++ index.js
@@ -6,11 +6,12 @@ const capslock = document.getElementById("key_capslock");
 const shift = document.getElementById("key_shift");
 const space = document.getElementById("key_space");
 const ok = document.getElementById("key_ok");
+const dark_white_theme_switcher = document.getElementById("switch_theme");
 
 
-const random_function = () =>{
+const random_function = () => {
 
-    console.log("Funkcija nek ima");    
+    console.log(`A photon checks into a hotel, where a bellhop asks where its suitcase is. The photon replies, “I didn’t bring any luggage. I’m traveling light."`);
 }
 
 random_function()
@@ -27,33 +28,59 @@ document.getElementById("show_keyboard").addEventListener("click", () => {
 virtual_keyboard_regular_keys.forEach(key => {
     key.addEventListener('click', () => {
         // textarea.value = textarea.value.slice(0, -1)
-        let char = key.value;
         textarea.focus();
-        if(shift.classList.contains("active")){
-            virtual_keyboard_regular_keys.forEach(item =>{
+        let char = key.value;
+        if (shift.classList.contains("active")) {
+            switch (key.value) {
+                case "1": char = "!";
+                    break;
+                case "2": char = "@";
+                    break;
+                case "3": char = "#";
+                    break;
+                case "4": char = "$";
+                    break;
+                case "5": char = "%";
+                    break;
+                case "6": char = "^";
+                    break;
+                case "7": char = "&";
+                    break;
+                case "8": char = "*";
+                    break;
+                case "9": char = "(";
+                    break;
+                case "0": char = ")";
+                    break;
+
+            }
+
+        }
+        if (shift.classList.contains("active")) {
+            virtual_keyboard_regular_keys.forEach(item => {
                 item.classList.toggle("toUpperText")
             })
         }
-    if ((shift.classList.contains('active') && !capslock.classList.contains('active')) || (!shift.classList.contains('active') && capslock.classList.contains('active'))) {
-        textarea.value += char.toUpperCase(); 
-        // textarea.value += char.toUpperCase() + "|"; kursor ? 
-        shift.classList.remove('active');
-    } else {
-        textarea.value += char;
-        shift.classList.remove('active');
-    }
-})
+        if ((shift.classList.contains('active') && !capslock.classList.contains('active')) || (!shift.classList.contains('active') && capslock.classList.contains('active'))) {
+            textarea.value += char.toUpperCase();
+            // textarea.value += char.toUpperCase() + "|"; kursor ? 
+            shift.classList.remove('active');
+        } else {
+            textarea.value += char;
+            shift.classList.remove('active');
+        }
+    })
 })
 
 // document.addEventListener("keydown",(e)=>{
 //     if(e.key === "Enter"){
 //         // virtual_keyboard_regular_keys.forEach(key =>{
 //             // if(key.clicked){
-                
+
 //                 // textarea.value = textarea.value.slice(0, -1)
 //                 textarea.value += "\n"
 //             // }
-            
+
 //         // })
 //     }
 // })
@@ -78,33 +105,35 @@ backspace.addEventListener('click', () => {
 shift.addEventListener('click', () => {
     textarea.focus();
     shift.classList.toggle('active');
-    virtual_keyboard_regular_keys.forEach(key =>{
-
+    document.querySelectorAll(".sup_number").forEach(item => item.classList.toggle("sup_number_active"))
+    document.querySelectorAll(".key_number").forEach(item => item.classList.toggle("key_number_active"))
+    console.log(document.querySelectorAll(".sup_number"));
+    virtual_keyboard_regular_keys.forEach(key => {
         key.classList.toggle("toUpperText")
-       
-     })
+
+    })
 })
 
 capslock.addEventListener('click', () => {
     textarea.focus();
-   capslock.classList.toggle("active")
-   virtual_keyboard_regular_keys.forEach(key =>{
+    capslock.classList.toggle("active")
+    virtual_keyboard_regular_keys.forEach(key => {
 
-       key.classList.toggle("toUpperText")
+        key.classList.toggle("toUpperText")
     })
-   
+
 })
 
 
 
 
-ok.addEventListener('click', () => {        
+ok.addEventListener('click', () => {
     alert(text.value);
     shift.classList.toggle("active")
     text.value = '';
 })
 
-document.getElementById("switch_theme").addEventListener("click",()=>{
+dark_white_theme_switcher.addEventListener("click", () => {
     console.log("Aa");
     document.querySelector(".keyboard").classList.toggle("dark_theme_keyboard")
     document.querySelector(".main").classList.toggle("dark_theme_body")
@@ -117,15 +146,15 @@ ok.addEventListener("click", () => {
 
 let control_;
 let enter_;
-document.addEventListener('keydown', (e) => {
+document.addEventListener("keydown", (e) => {
     console.log(e.key);
-    if(e.key == "Control") {
+    if (e.key === "Control") {
         control_ = true;
     }
-    if(e.key == "Enter") {
+    if (e.key === "Enter") {
         enter_ = true;
     }
-    if(control_ && enter_) {
+    if (control_ && enter_) {
 
         console.log("Aaaa");
         alert(`${textarea.value}`)
@@ -133,5 +162,6 @@ document.addEventListener('keydown', (e) => {
         control_ = false;
         enter_ = false;
     }
- });
- 
+});
+
+
